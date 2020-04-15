705daf5505738b7e1ee2a313d080ee4469ea1d90
Xueli Chen <v-xuelc@microsoft.com>
2020-04-15 10:23:19 +0800

    first commit

---
+++ DBRepository/MyDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBRepository
+{
+    public class MyDbContext:DbContext
+    {
+        public MyDbContext (DbContextOptions<MyDbContext> options):base(options)
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=WebAppCore;Trusted_Connection=True;ConnectRetryCount=0");
+
+            }
+        }
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ WebAPPCore/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DBRepository;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace WebAPPCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly MyDbContext _context;
+          
+        public HomeController(MyDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var model = _context.User.ToList();
+            return View(model);
+        }
+    }
+}
